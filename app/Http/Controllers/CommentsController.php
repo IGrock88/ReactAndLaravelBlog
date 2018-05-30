99099e5721e2ab62df794899f1d38f968f8497db
IGrock88 <igrock88@gmail.com>
2018-05-30 15:31:47 +0800

    added Controller for Comments and Comments model

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comments;
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    public function showCommentsByPost(Request $request)
+    {
+
+    }
+
+}
