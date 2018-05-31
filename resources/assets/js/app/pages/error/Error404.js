0191227786280c527b6da050cff3c5f869941685
IGrock88 <igrock88@gmail.com>
2018-05-31 14:08:37 +0800

    delete unused js files

--- resources/assets/js/app/pages/error/Error404.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default class Error404 extends React.Component{
-
-    render(){
-        return <div className='col-lg-8'><h1>Error 404. Page not found</h1></div>
-    }
-}
--- resources/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-import 'app/styles/jumbotron.css';
-import 'app/styles/animationLoading.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-let index = require("app/modules/app");
-index('#root');
